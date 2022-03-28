36f5f67c4ea45d0e3ea046420d42abb2c62d0802
Christophe Diederichs <chris.d.itunes@gmail.com>
2022-03-28 18:58:00 +0200

    add test using hypercore extension

--- test/auth.js
+++ test/auth.js
@@ -1,7 +1,7 @@
 const test = require('brittle')
 const ram = require('random-access-memory')
 const crypto = require('hypercore-crypto')
-const { replicate } = require('./helpers')
+const { eventFlush, replicate } = require('./helpers')
 
 const Hypercore = require('../')
 
@@ -51,5 +51,92 @@ test('multisig hypercore', async function (t) {
   await r.downloaded()
 
   t.is(b.length, 5)
+  t.end()
+})
+
+test('multisig hypercore with extension', async function (t) {
+  t.plan(2)
+
+  const aKey = crypto.keyPair()
+  const bKey = crypto.keyPair()
+
+  const sigs = []
+
+  const auth = {
+    sign: (signable) => {
+      const remote = sigs.find(findBySignable)
+      const local = crypto.sign(signable, aKey.secretKey)
+
+      return Buffer.concat([local, Buffer.from(remote.signature, 'base64')])
+
+      function findBySignable ({ block }) {
+        const batch = a.core.tree.batch()
+        batch.append(a._encode(a.valueEncoding, data))
+        return Buffer.compare(batch.signable(), signable) === 0
+      }
+    },
+    verify: (signable, signature) => {
+      const sig1 = signature.slice(0, 64)
+      const sig2 = signature.slice(64)
+
+      return crypto.verify(signable, sig1, aKey.publicKey) &&
+        crypto.verify(signable, sig2, bKey.publicKey)
+    }
+  }
+
+  const a = new Hypercore(ram, null, {
+    valueEncoding: 'utf-8',
+    auth
+  })
+
+  const b = new Hypercore(ram, a.key, {
+    valueEncoding: 'utf-8',
+    auth: {
+      ...auth,
+      sign: null
+    }
+  })
+
+  await a.ready()
+  await b.ready()
+
+  const data = 'hello'
+
+  a.registerExtension('multisig-extension', {
+    encoding: 'json',
+    onmessage: (message, peer) => {
+      sigs.push(message)
+    }
+  })
+
+  const ext = b.registerExtension('multisig-extension', {
+    encoding: 'json',
+    onmessage: (message, peer) => {
+    }
+  })
+
+  replicate(a, b, t)
+
+  await eventFlush()
+
+  const batch = b.core.tree.batch()
+  batch.append(b._encode(b.valueEncoding, data))
+
+  const signable = batch.signable()
+  const signature = crypto.sign(signable, bKey.secretKey).toString('base64')
+
+  ext.send({ data, signature, length: a.length }, b.peers[0])
+
+  await eventFlush()
+
+  await a.append('hello')
+
+  t.is(a.length, 1)
+
+  const r = b.download({ start: 0, end: a.length })
+  await r.downloaded()
+
+  t.is(a.length, 1)
+
   t.end()
 })
