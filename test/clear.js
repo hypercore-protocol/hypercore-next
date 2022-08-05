f07c04a5a9631bfc0177c3959cd6f6a3c429d275
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-05 09:40:00 +0200

    Update contig length on clear

--- test/clear.js
+++ test/clear.js
@@ -5,9 +5,13 @@ test('clear', async function (t) {
   const a = await create()
   await a.append(['a', 'b', 'c'])
 
+  t.is(a.contiguousLength, 3)
+
   await a.clear(1)
 
-  t.ok(await a.has(0))
-  t.absent(await a.has(1))
-  t.ok(await a.has(2))
+  t.is(a.contiguousLength, 1, 'contig updated')
+
+  t.ok(await a.has(0), 'has 0')
+  t.absent(await a.has(1), 'has not 1')
+  t.ok(await a.has(2), 'has 2')
 })
