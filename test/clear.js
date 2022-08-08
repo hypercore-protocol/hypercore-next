46271bdc4c1ae4aab70cb87e7773c1d982bdb497
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-08 11:37:37 +0200

    Test gossiping

--- test/clear.js
+++ test/clear.js
@@ -16,7 +16,7 @@ test('clear', async function (t) {
   t.ok(await a.has(2), 'has 2')
 })
 
-test('clear during replication', async function (t) {
+test('clear + replication', async function (t) {
   const a = await create()
   const b = await create(a.key)
 
@@ -28,6 +28,27 @@ test('clear during replication', async function (t) {
   await a.clear(1)
 
   t.absent(await a.has(1), 'a cleared')
+  t.ok(await b.has(1), 'b not cleared')
 
   t.alike(await a.get(1), Buffer.from('b'), 'a downloaded from b')
+})
+
+test('clear + replication, gossip', async function (t) {
+  const a = await create()
+  const b = await create(a.key)
+  const c = await create(a.key)
+
+  replicate(a, b, t)
+  replicate(b, c, t)
+
+  await a.append(['a', 'b', 'c'])
+  await b.download({ start: 0, end: 3 }).downloaded()
+  await c.update()
+
+  await b.clear(1)
+
+  t.ok(await a.has(1), 'a not cleared')
+  t.absent(await b.has(1), 'b cleared')
+
+  t.alike(await c.get(1), Buffer.from('b'), 'c downloaded from a')
 })
