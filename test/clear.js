3b401a0a1a8a65ffcab583947446a05c87ba6c57
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-08 11:30:57 +0200

    Test simple replication

--- test/clear.js
+++ test/clear.js
@@ -1,5 +1,5 @@
 const test = require('brittle')
-const { create } = require('./helpers')
+const { create, replicate } = require('./helpers')
 
 test('clear', async function (t) {
   const a = await create()
@@ -14,4 +14,20 @@ test('clear', async function (t) {
   t.ok(await a.has(0), 'has 0')
   t.absent(await a.has(1), 'has not 1')
   t.ok(await a.has(2), 'has 2')
+})
+
+test('clear during replication', async function (t) {
+  const a = await create()
+  const b = await create(a.key)
+
+  replicate(a, b, t)
+
+  await a.append(['a', 'b', 'c'])
+  await b.download({ start: 0, end: 3 }).downloaded()
+
+  await a.clear(1)
+
+  t.absent(await a.has(1), 'a cleared')
+
+  t.alike(await a.get(1), Buffer.from('b'), 'a downloaded from b')
 })
